bb9cc8870fa0e32411c2290eb911906649f9ee7f
Abdullah Al Masoud <almasoud49@yahoo.com>
2024-05-04 22:02:57 +0600

    [DONE] vercel

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 5000;
 
 //Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      // "http://localhost:5173",
+
+      'https://touristatravels-d9cb9.web.app',
+      'https://touristatravels-d9cb9.firebaseapp.com'
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+app.use(cookieParser());
 
 //MongodbClient
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@personal.dp2qomu.mongodb.net/?retryWrites=true&w=majority&appName=personal`;
@@ -22,23 +35,19 @@ const client = new MongoClient(uri, {
 });
 
 //Verify JWT Token
-
 const verifyJwtToken = (req, res, next) => {
-  const authToken = req.headers?.authorization;
-  if (!authToken) {
-    return res
-      .status(401)
-      .send({ error: true, message: "unauthorized access" });
+  const authHeader = req?.headers?.authorization;
+  const token = authHeader?.split(" ")[1];
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized Access" });
   }
-  jwt.verify(authToken, process.env.JWT_ACCESS_TOKEN, (error, decoded) => {
-    if (error) {
-      return res
-        .status(401)
-        .send({ error: true, message: "unauthorized access" });
-    }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_TOKEN);
     req.decoded = decoded;
-    next();
-  });
+  } catch (err) {
+    return res.status(403).send({ message: "Access Forbidden" });
+  }
+  next();
 };
 
 const touristaTravels = async () => {
@@ -47,7 +56,7 @@ const touristaTravels = async () => {
     const database = client.db("touristaTravels");
     const serviceCollection = database.collection("services");
     const reviewCollection = database.collection("reviews");
-    const testimonialCollection = database.collection('testimonials');
+    const blogCollection = database.collection("blogs");
     const userCollection = database.collection("users");
 
     //JWT create
@@ -68,184 +77,205 @@ const touristaTravels = async () => {
 
     //Get all  Services
     app.get("/services", async (req, res) => {
-        const limit = parseInt(req.query.limit);
-		const page = parseInt(req.query.page);
-		const size = parseInt(req.query.size);
-		const query = {};
-        const count = await serviceCollection.estimatedDocumentCount();
-		if (limit) {
-			const services = await serviceCollection
-				.find(query)
-				.sort({ createAt: -1 })
-				.limit(limit)
-				.toArray();
-			res.send(services);
-		}else {
-			const result = await serviceCollection
-				.find(query)
-				.sort({ createAt: -1 })
-				.skip(page * size)
-				.limit(size)
-				.toArray();
-			res.send({ count, result });
-		}
-    
+      const limit = parseInt(req.query.limit);
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const query = {};
+      const count = await serviceCollection.estimatedDocumentCount();
+      if (limit) {
+        const result = await serviceCollection
+          .find(query)
+          .sort({ createAt: -1 })
+          .limit(limit)
+          .toArray();
+        res.send(result);
+      } else {
+        const result = await serviceCollection
+          .find(query)
+          .sort({ createAt: -1 })
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+        res.send({ count, result });
+      }
     });
 
     //Get User Created Service
-
-    app.get('/my-service',verifyJwtToken, async(req,res)=>{
-        const decoded = req.decoded;
-		const userId = req.query.userId;
-		const query = { createBy: userId };
-		if (decoded.userId !== userId) {
-			return res.status(403).send({ message: 'Access Forbidden' });
-		}
-        const result = await serviceCollection
-			.find(query)
-			.sort({ createAt: -1 })
-			.toArray();
-		const count = await serviceCollection.countDocuments(query);
-		res.send({ count, result });
-    })
+    app.get("/my-service", verifyJwtToken, async (req, res) => {
+      const decoded = req.decoded;
+      const uid = req.query.uid;
+      const query = { createBy: uid };
+      if (decoded.uid !== uid) {
+        return res.status(403).send({ message: "Access Forbidden" });
+      }
+      const result = await serviceCollection
+        .find(query)
+        .sort({ createAt: -1 })
+        .toArray();
+      const count = await serviceCollection.countDocuments(query);
+      res.send({ count, result });
+    });
 
     //Create a service
-    app.post("/services", verifyJwtToken,async (req, res) => {
-        const decoded = req.decoded;
+    app.post("/services", verifyJwtToken, async (req, res) => {
+      const decoded = req.decoded;
       const service = req.body;
-      const userId= req.query.userId;
-      if (decoded.userId !== userId) {
-        return res.status(403).send({ message: 'Access Forbidden' });
-    }
+      const uid = req.query.uid;
+      if (decoded.uid !== uid) {
+        return res.status(403).send({ message: "Access Forbidden" });
+      }
+      const result = await serviceCollection.insertOne(service);
+      res.send(result);
+    });
+
+    //add service
+    app.post("/add-service", verifyJwtToken, async (req, res) => {
+      const decoded = req.decoded;
+      const service = req.body;
+      const uid = req.query.uid;
+      if (decoded.uid !== uid) {
+        return res.status(403).send({ message: "Access Forbidden" });
+      }
       const result = await serviceCollection.insertOne(service);
       res.send(result);
     });
 
     //Get a  Single Service
     app.get("/service/:id", async (req, res) => {
-      const id = req.params._id;
-      const result = await serviceCollection.findOne(id);
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
       res.send(result);
     });
 
     //Post a review
-    app.post("/review",verifyJwtToken, async (req, res) => {
-        const decoded = req.decoded;
-        const review = req.body;
-        const userId = req.query.userId;
-		if (decoded.userId !== userId) {
-			return res.status(403).send({ message: 'Access Forbidden' });
-		}
+    app.post("/review", verifyJwtToken, async (req, res) => {
+      const decoded = req.decoded;
+      const review = req.body;
+      const uid = req.query.uid;
+      if (decoded.uid !== uid) {
+        return res.status(403).send({ message: "Access Forbidden" });
+      }
       const result = await reviewCollection.insertOne(review);
       res.send(result);
     });
 
     //Get all review
     app.get("/reviews", async (req, res) => {
-        const service_id = req.query.service_id;
-		const page = parseInt(req.query.page);
-		const size = parseInt(req.query.size);
-		const query = { service_id: service_id };
-      const result = await reviewCollection.find(query)
-      .sort({ createAt: -1 })
-      .skip(page * size)
-      .limit(size)
-      .toArray();
-      res.send(result);
+      const service_id = req.query.service_id;
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const query = { service_id: service_id };
+      const reviews = await reviewCollection
+        .find(query)
+        .sort({ createAt: -1 })
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+
       const reviewsRating = await reviewCollection.find(query).toArray();
-		const count = await reviewCollection.countDocuments(query);
-		const sum = reviewsRating.reduce(
-			(pre, cur) => pre + cur.user_rating,
-			0
-		);
-		let average = 0;
-		if (!isNaN(sum / count)) {
-			average = sum / count;
-		}
-		res.send({ count, reviews, average });
+      const count = await reviewCollection.countDocuments(query);
+      const sum = reviewsRating.reduce((pre, cur) => pre + cur.user_rating, 0);
+      let average = 0;
+      if (!isNaN(sum / count)) {
+        average = sum / count;
+      }
+      res.send({ count, reviews, average });
     });
 
     //Get Single review
-    app.get("/my-review",verifyJwtToken, async (req, res) => {
-        const decoded = req.decoded;
-		const userId = req.query.userId;
-		const query = { user_userId: userId };
-		if (decoded.userId !== userId) {
-			return res.status(403).send({ message: 'Access Forbidden' });
-		} else {
-			const result = await reviewCollection
-				.find(query)
-				.sort({ createAt: -1 })
-				.toArray();
-			const count = await reviewCollection.countDocuments(query);
-			res.send({ count, result });
-		}
+    app.get("/my-review", verifyJwtToken, async (req, res) => {
+      const decoded = req.decoded;
+      const uid = req.query.uid;
+      const query = { user_uid: uid };
+      if (decoded.uid !== uid) {
+        return res.status(403).send({ message: "Access Forbidden" });
+      } else {
+        const reviews = await reviewCollection
+          .find(query)
+          .sort({ createAt: -1 })
+          .toArray();
+        const count = await reviewCollection.countDocuments(query);
+        console.log(count);
+        res.send({ count, reviews });
+      }
     });
 
     //Update review item
-    app.patch("/my-review/:id",verifyJwtToken , async (req, res) => {
-        const decoded = req.decoded;
-		const id = req.query.id;
-		const userId = req.query.userId;
-		const review = req.body;
-		const filter = { _id:new ObjectId(id) };
-		const updatedReview = {
-			$set: review,
-		};
-		if (decoded.userId !== userId) {
-			return res.status(403).send({ message: 'Access Forbidden' });
-		} else {
-			const result = await reviewCollection.updateOne(
-				filter,
-				updatedReview
-			);
-			res.send(result);
-		}
+    app.patch("/my-review", verifyJwtToken, async (req, res) => {
+      const decoded = req.decoded;
+      const id = req.query.id;
+      const uid = req.query.uid;
+      const review = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedReview = {
+        $set: review,
+      };
+      if (decoded.uid !== uid) {
+        return res.status(403).send({ message: "Access Forbidden" });
+      } else {
+        const result = await reviewCollection.updateOne(filter, updatedReview);
+        res.send(result);
+      }
     });
 
     //Delete a single review
-    app.delete("/my-review" ,verifyJwtToken, async (req, res) => {
-        const decoded = req.decoded;
-		const id = req.query.id;
-		const userId = req.query.userId;
-		const query = { _id: new ObjectId(id)};
-		if (decoded.userId !== userId) {
-			return res.status(403).send({ message: 'Access Forbidden' });
-		} else {
-			const result = await reviewCollection.deleteOne(query);
-			res.send(result);
-		}
+    app.delete("/my-review", verifyJwtToken, async (req, res) => {
+      const decoded = req.decoded;
+      const id = req.query.id;
+      const uid = req.query.uid;
+      const query = { _id: new ObjectId(id) };
+      if (decoded.uid !== uid) {
+        return res.status(403).send({ message: "Access Forbidden" });
+      } else {
+        const result = await reviewCollection.deleteOne(query);
+        res.send(result);
+      }
     });
 
     //Get all Testimonial
-	app.get('/testimonials', async (req, res) => {
-		const query = {};
-		const result = await reviewCollection.find(query).sort({createAt: -1}).limit(4).toArray();
-		res.send(result);
-	});
+    app.get("/testimonials", async (req, res) => {
+      const query = {};
+      const result = await reviewCollection
+        .find(query)
+        .sort({ createAt: -1 })
+        .limit(4)
+        .toArray();
+      res.send(result);
+    });
 
-	//Get all user
-	app.get('/users', async (req, res) => {
-		const query = {};
-		const result = await userCollection.find(query).toArray();
-		res.send(result);
-	});
+    //Create blogs
+    // app.post('/blogs' , async(req,res)=>{
+    //   const blogsData = req.body;
+    //   const result = await blogCollection.insertOne(blogsData);
+    //   res.send(result);
+    // });
 
-  //Add New User To Database
-	app.post('/users', async (req, res) => {
-		const user = req.body;
-		const option = { upsert: true };
-		const query = { userId: user.userId };
-		const userInDb = await userCollection.findOne(query);
-		if (userInDb?.userId) {
-			return;
-		}
-		const result = await userCollection.insertOne(user, option);
-		res.send(result);
-	});
+    //get blogs data
+    app.get("/blogs", async (req, res) => {
+      const result = await blogCollection.find().toArray();
+      res.send(result);
+    });
 
+    //Get all user
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const result = await userCollection.find(query).toArray();
+      res.send(result);
+    });
 
-
+    //Add New User To Database
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const option = { upsert: true };
+      const query = { uid: user.uid };
+      const userInDb = await userCollection.findOne(query);
+      if (userInDb?.uid) {
+        return;
+      }
+      const result = await userCollection.insertOne(user, option);
+      res.send(result);
+    });
   } finally {
   }
 };
